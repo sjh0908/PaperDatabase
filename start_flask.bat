8f2be696a03a1f599e7d146b560c705be70b03ef
sjh0908 <2576625902@qq.com>
2026-07-01 15:46:37 +0800

    Add start_flask.bat

---
+++ start_flask.bat
@@ -0,0 +1,4 @@
+@echo off
+set MYSQL_PASSWORD=123456
+cd /d "C:\Users\s\Desktop\PaperDatabase"
+"C:\Users\s\Desktop\PaperDatabase\.venv\Scripts\python.exe" "C:\Users\s\Desktop\PaperDatabase\app.py" > "C:\Users\s\Desktop\PaperDatabase\flask.log" 2>&1
